1fee432cec8b6ed4ad879bcbf835ac12f9612d75
Dylan Sewell <dvsewell15@gmail.com>
2019-08-12 11:01:27 -0700

    Initial Commit

---
+++ SimpleTableViewController-MVC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleTableViewController-MVC
+//
+//  Created by Dylan Sewell on 8/12/19.
+//  Copyright © 2019 dylansewell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleTableViewController-MVC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimpleTableViewController-MVC
+//
+//  Created by Dylan Sewell on 8/12/19.
+//  Copyright © 2019 dylansewell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
